4318596be338bab5f514ba70f1b0f7a00a499b7b
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-11-18 00:17:06 -0400

    Added initial project settings and initial layout

---
+++ app/src/main/java/com/solitudeworks/helius/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.solitudeworks.helius
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
